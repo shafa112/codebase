e02153f7aa571a18c49f841dc8eca5c6392a01d6
shafa <shafa.hassan112@gmail.com>
2018-06-06 16:37:41 +0530

    stack update

--- datastructure/src/datastructure/stack/Stack.java
+++ datastructure/src/datastructure/stack/Stack.java
@@ -2,13 +2,18 @@ package datastructure.stack;
 
 public class Stack {
 	
-	int[] stack = new int[10];
+	int[] stack = new int[5];
 	int top=-1;
 	int size;
 	public static void main(String[] args) {
+		
 		Stack s = new Stack();
 		s.push(20);
 		s.push(27);
+		s.push(100);
+		s.push(19);
+		s.push(28);
+		s.push(10);
 		s.display();
 		s.peek();
 		s.pop();
@@ -16,20 +21,23 @@ public class Stack {
 		s.display();
 		
 	}
-	public void display() {
+	public void display()
+	{
 		for(int i=0;i<stack.length;++i)
 			System.out.print(stack[i]+" ");
 		System.out.println();
 		
 	}
+	
 	public void push(int i) {
 		
-		if(top==10) {
+		if(top+1>=5) {
 			System.out.println("Stack full");
 			return;
 		}
 		stack[++top]=i;
 	}
+	
 	public int pop()
 	{
 		if(isEmpty()!=-1)
